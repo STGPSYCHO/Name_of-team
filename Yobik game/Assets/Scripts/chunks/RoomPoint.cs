1b906fbe0515a859b7bcd4ea993d017e68bafdaf
Andrey Bubenko <a.bubenko@mail.ru>
2022-12-18 13:54:03 +0700

    Procedure Generation ready

---
+++ Yobik game/Assets/Scripts/chunks/RoomPoint.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using static RoomTrigger;
+
+public class RoomPoint : MonoBehaviour
+{
+    public DirectionPoint Direction_Point;
+
+    public enum DirectionPoint
+    {
+        center,
+        top_left,
+        top_right,
+        top,
+        left,
+        right,
+        bottom,
+        bottom_left,
+        bottom_right,
+    }
+}
